ddb1b293c8da8d884b22bf874e05a604d8c96304
SisoroT <rtaylor6410@gmail.com>
2022-03-20 18:05:39 -0400

    reorganized parameters and fixed default return statement

--- datastructures/python/classwork/BinarySearch.py
+++ datastructures/python/classwork/BinarySearch.py
@@ -1,14 +1,14 @@
-def binary_search_recursive(arr: list, target: int, low: int, high: int):
+def binary_search_recursive(arr: list, low: int, high: int, target: int):
     mid = (low + high) // 2
     if low <= high:
         if arr[mid] == target:
             return mid
         if arr[mid] < target:
-            return binary_search_recursive(arr, target, mid + 1, high)
+            return binary_search_recursive(arr, mid + 1, high, target)
         if arr[mid] > target:
-            return binary_search_recursive(arr, target, low, mid - 1)
-        return -1
+            return binary_search_recursive(arr, low, mid - 1, target)
+    return -1
 
 
 arr = [0, 1, 2, 3, 5, 7, 9, 12, 13]
-print(f"The target is found at index {binary_search_recursive(arr, 9, 0, len(arr))}")
+print(f"The target is found at index {binary_search_recursive(arr, 0, len(arr), 9)}")
