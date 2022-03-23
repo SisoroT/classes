7813eda313321e928495a21a433c56303f156975
SisoroT <rtaylor6410@gmail.com>
2022-03-22 20:13:12 -0400

    made progress on a couple different things

--- datastructures/python/labs/lab3_2dArrayRotation/Rotation.py
+++ datastructures/python/labs/lab3_2dArrayRotation/Rotation.py
@@ -1,31 +1,15 @@
-import java.util.*;
+def clockwise_rotation(arr: list[list[int]]):
+    new_arr = arr.copy()
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            new_arr[i][j] = arr[len(arr[0]) - 1 - j][i]
 
-public class Rotation {
-    public static void main(String[] args) {
-        int[][] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+    arr = new_arr
 
-        System.out.println("Before Rotation: " + Arrays.deepToString(arr));
-        ClockwiseRotation(arr);
-        System.out.println("After Rotation: " + Arrays.deepToString(arr));
-    }
 
-    public static void ClockwiseRotation(int[][] arr) {
-        int[][] newArr = new int[arr.length][arr[0].length];
+if __name__ == "__main__":
+    arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 
-        // rewrite the rows of newArr as the columns of arr
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                newArr[i][j] = arr[arr[0].length - 1 - j][i];
-                // System.out.print(newArr[i][j] + " ");
-            }
-            // System.out.println();
-        }
-
-        // overwrite arr with the contents of newArr
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                arr[i][j] = newArr[i][j];
-            }
-        }
-    }
-}
+    print(f"Before Rotation: {arr}")
+    clockwise_rotation(arr)
+    print(f"After Rotation: {arr}")
