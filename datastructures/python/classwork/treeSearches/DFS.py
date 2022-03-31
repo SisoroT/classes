f2f7331ea78e76a4981a03b50bf2577aed7e1e78
SisoroT <rtaylor6410@gmail.com>
2022-03-31 14:32:20 -0400

    finished BFS and DFS tree algorithms

--- datastructures/python/classwork/treeSearches/DFS.py
+++ datastructures/python/classwork/treeSearches/DFS.py
@@ -3,3 +3,28 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
+
+
+# function to do inorder tree traversal
+def print_in_order(root):
+    if root:
+        # recur on left child
+        print_in_order(root.left)
+        # print the data of node
+        print(root.data)
+        # recur on right child
+        print_in_order(root.right)
+
+
+if __name__ == "__main__":
+
+    root = Node(4)
+    root.left = Node(2)
+    root.right = Node(6)
+    root.left.left = Node(1)
+    root.left.right = Node(3)
+    root.right.left = Node(5)
+    root.right.right = Node(7)
+
+    print("Inorder traversal of binary tree is")
+    print_in_order(root)
