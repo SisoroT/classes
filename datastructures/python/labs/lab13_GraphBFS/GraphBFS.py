3df4f8bf137dfb63b395ddc16ec14d16353b10b8
SisoroT <rtaylor6410@gmail.com>
2022-04-06 22:28:53 -0400

    completed lab 13

--- datastructures/python/labs/lab13_GraphBFS/GraphBFS.py
+++ datastructures/python/labs/lab13_GraphBFS/GraphBFS.py
@@ -11,6 +11,31 @@ class Graph:
         """Function to add an edge to the graph from vertex1 to vertex2"""
         self.graph[vertex1].append(vertex2)
 
+    def bfs(self, node):
+        """Prints BFS traversal of a graph given a starting node."""
+        # create a set to store all visited vertices
+        visited = set()
+
+        # create a queue for BFS
+        queue = []
+
+        # enqueue the source node and mark the it as visited
+        queue.append(node)
+        visited.add(node)
+
+        while queue:
+
+            # dequeue a node from the queue and print it
+            node = queue.pop(0)
+            print(node, end=" ")
+
+            # if an adjacent node hasn't been visited,
+            # enqueue it and mark it visited
+            for neighbor in self.graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+
 
 if __name__ == "__main__":
     graph = Graph()
@@ -23,4 +48,4 @@ if __name__ == "__main__":
     graph.add_edge(4, 5)
 
     print("Breadth First Traversal")
-    # graph.bfs(0)
+    graph.bfs(0)
