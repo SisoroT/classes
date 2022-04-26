4d66aebfb115fe2b8361a360f99116d0a13a4335
SisoroT <rtaylor6410@gmail.com>
2022-04-26 03:34:34 -0400

    changed tile flip speed

--- gamedev/wordle_royale_project/app.js
+++ gamedev/wordle_royale_project/app.js
@@ -704,6 +704,6 @@ const flipTile = () => {
             tile.classList.add('flip')
             tile.classList.add(guess[index].color)
             addColorToKey(guess[index].letter, guess[index].color)
-        }, 500 * index)
+        }, 300 * index)
     })
 }
