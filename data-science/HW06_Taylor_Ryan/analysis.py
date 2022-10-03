3d1d1a96dbb5711811f1b15662e24ea9b625ddc4
SisoroT <rtaylor6410@gmail.com>
2022-10-02 21:46:23 -0400

    completed hw6

--- data-science/HW06_Taylor_Ryan/analysis.py
+++ data-science/HW06_Taylor_Ryan/analysis.py
@@ -19,7 +19,7 @@ labels_basic = df.columns[:-1]
 Y = df.values[:, -1]
 
 # Expand to a 2-degree polynomials
-## Your code here
+poly = PolynomialFeatures(2)
 
 # Prepare for loop
 residual = Y
@@ -29,8 +29,35 @@ residual = Y
 feature_indices = [0]
 
 while len(feature_indices) < 3:
-    pass
-    ## Your code here
+    p_values = []
+
+    # Calculate the p-value of the Pearson correlation between each input and the current residual
+    for i in range(len(X_basic[0])):
+        p_values.append(pearsonr(X_basic[:, i], residual)[1])
+        print(f"\t{df.columns[i]} vs residual: p-value={p_values[i]}")
+
+    p_values = np.array(p_values)
+
+    # Sort the inputs so that the p-values are in ascending order
+    sorted_indices = np.argsort(p_values)
+
+    # Add the input with the lowest p-value to the list of inputs you are going to actually use
+    feature_indices.append(sorted_indices[0])
+    X = X_basic[:, feature_indices]
+
+    # create list of features in use
+    features = df.columns[feature_indices].to_list()
+
+    # Do linear regression using that list of inputs
+    print(f'**** Fitting with ["{feature_indices[1:]}" "{features[1:]}"] ****')
+    model = LinearRegression()
+    model.fit(X, Y)
+
+    # Print the R2 value
+    print(f"R2: {model.score(X, Y)}")
+    # Calculate a new residual
+    residual = Y - model.predict(X)
+    print("Residual is updated\n")
 
 # Any relationship between the final residual and the unused variables?
 print("Making scatter plot: age_of_roof vs final residual")
--- data-science/HW06_Taylor_Ryan/prediction.py
+++ data-science/HW06_Taylor_Ryan/prediction.py
@@ -13,4 +13,27 @@ df = pd.read_csv(infilename, index_col="property_id")
 
 print("Making new features...")
 
-## Your code here
+
+# save prices column as label
+labels = df.values[:, -1]
+# create new feature
+df["is_close_to_school"] = df["miles_to_school"] < 2
+df["lot_size"] = df["lot_width"] * df["lot_depth"]
+# extract features to use
+cols = df[["sqft_hvac", "lot_size", "is_close_to_school"]]
+col_names = cols.columns.to_list()
+features = cols.values
+
+print(f"Using only the useful ones: {col_names}...")
+# train model
+model = LinearRegression()
+model.fit(features, labels)
+print(f"R2 = {model.score(features, labels):.5f}")
+
+print("*** Prediction ***")
+print(
+    f"Price = ${model.intercept_:,.2f} + (sqft x ${model.coef_[0]:,.2f}) + (lot_size x ${model.coef_[1]:,.2f})"
+)
+print(
+    f"\tLess than 2 miles from a school? You get ${model.coef_[-1]:,.2f} added to the price!"
+)
