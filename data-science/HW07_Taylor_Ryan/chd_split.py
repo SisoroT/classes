522e6b2a37cfa9f5c4e331e8430ab3e24f32e758
SisoroT <rtaylor6410@gmail.com>
2022-10-07 13:57:40 -0400

    completed hw7

--- data-science/HW07_Taylor_Ryan/chd_split.py
+++ data-science/HW07_Taylor_Ryan/chd_split.py
@@ -5,17 +5,13 @@ df = pd.read_csv("framingham.csv", index_col=False)
 print(f"Read {df.shape[0]} rows")
 
 # Drop rows with missing values
-## Your code here
 df = df.dropna()
 
 print(f"Using {df.shape[0]} rows")
 
 # Split into training and testing dataframes
-## Your code here
 train, test = train_test_split(df, test_size=0.2)
 
-
 # Write out each as a CSV
-## Your code here
 train.to_csv("train.csv", index=False)
 test.to_csv("test.csv", index=False)
--- data-science/HW07_Taylor_Ryan/chd_test.py
+++ data-science/HW07_Taylor_Ryan/chd_test.py
@@ -8,38 +8,33 @@ import pickle
 import logging
 
 # Configure logger
-## Your code here
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
     "%(levelname)s@%(asctime)s: %(message)s", datefmt="%H:%M:%S"
 )
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # Read in test data
-## Your code here
 df = pd.read_csv("test.csv")
 X_basic = df.values[:, :-1]
 Y = df.values[:, -1]
 
 # Read in model
-## Your code here
 with open("classifier.pkl", "rb") as f:
-    model = pickle.load(f)
+    scaler, logreg = pickle.load(f)
 
 # Scale X
-## Your code here
-scaler = StandardScaler()
 X = scaler.fit_transform(X_basic)
 
 # Check accuracy on test data
-## Your code here
-test_accuracy = model.score(X, Y)
+test_accuracy = logreg.score(X, Y)
 print(f"Test Accuracy = {test_accuracy}")
 
 # Show confusion matrix
-## Your code here
-pred = model.predict(X)
+pred = logreg.predict(X)
 cm = confusion_matrix(Y, pred)
 print(f"Confusion matrix = \n{cm}")
 
@@ -50,13 +45,18 @@ thresholds = []
 recall_scores = []
 precision_scores = []
 f1_scores = []
+best_threshold = 0.0
 while threshold <= 1.0:
     thresholds.append(threshold)
-    ## Your code here
-    pred = model.predict_proba(X)[:, 1] > threshold
+    pred = logreg.predict_proba(X)[:, 1] > threshold
+    accuracy = logreg.score(X, pred)
     recall = recall_score(Y, pred)
-    precision = precision_score(Y, pred)
+    precision = precision_score(Y, pred, zero_division=1)
     f1 = 2 * (precision * recall) / (precision + recall)
+
+    if f1 > best_f1:
+        best_f1 = f1
+        best_threshold = threshold
 
     recall_scores.append(recall)
     precision_scores.append(precision)
@@ -67,18 +67,18 @@ while threshold <= 1.0:
     threshold += 0.02
 
 # Make a confusion matrix for the best threshold
-## Your code here
-best_threshold = thresholds[np.argmax(f1_scores)]
-pred = model.predict_proba(X)[:, 1] > best_threshold
+pred = logreg.predict_proba(X)[:, 1] > best_threshold
 cm = confusion_matrix(Y, pred)
+print(f"\nBest threshold = {best_threshold:.3f}")
 print(f"Confusion matrix = \n{cm}")
+
 
 # Plot recall, precision, and F1 vs Threshold
 fig, ax = plt.subplots()
 ax.plot(thresholds, recall_scores, "b", label="Recall")
-ax.plot(thresholds, precision_scores, "g", label="Precision", color="g")
-ax.plot(thresholds, f1_scores, "r", label="F1", color="r")
-ax.vlines(best_threshold, 0, 1, "r", linewidth=0.5, linestyle="dashed")
+ax.plot(thresholds, precision_scores, label="Precision", color="g")
+ax.plot(thresholds, f1_scores, label="F1", color="r")
+ax.vlines(best_threshold, 0, 1, color="r", linewidth=0.5, linestyle="dashed")
 ax.set_xlabel("Threshold")
 ax.legend()
 fig.savefig("threshold.png")
