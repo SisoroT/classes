53738390d2ae4485bebc376920fc23ae7571c55e
SisoroT <rtaylor6410@gmail.com>
2023-01-14 19:16:49 -0500

    completed first assignment

---
+++ web-programming/section2/functions.js
@@ -0,0 +1,7 @@
+function createAlert() {
+    alert('This is an alert!')
+}
+function updateBody() {
+    document.getElementById('content-page').innerHTML =
+        'Here is new text for the body! These words are never before seen!'
+}
